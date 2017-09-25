73dbd68b2255dd1290615d53f27d2639a8395d87
ASDA-Grocery <32156105+ASDA-Grocery@users.noreply.github.com>
2017-09-25 18:46:37 +0530

    segregating data in separate files

---
+++ orderDb.js
@@ -0,0 +1,93 @@
+function getOrderTime(diff){
+  var currentDate = new Date()
+  currentDate.setTime(currentDate.getTime() + (diff*60000));
+  return currentDate;
+}
+
+exports.orderDb = [
+  {
+    orderId: 'OR100001',
+    productList: [
+      {
+        productId: 'PR100001',
+        productName: 'product1',
+        quantity: '2'
+      }
+    ],
+    orderPlacementDate: 'June 23, 2017',
+    value: '20 £',
+    status: 'closed',
+    deliveryTime: getOrderTime(30)
+  },
+  {
+    orderId: 'OR100002',
+    productList: [
+      {
+        productId: 'PR100001',
+        productName: 'product1',
+        quantity: '2'
+      },
+      {
+        productId: 'PR100004',
+        productName: 'product4',
+        quantity: '1'
+      }
+    ],
+    orderPlacementDate: 'July 2, 2017',
+    value: '35 £',
+    status: 'closed',
+    deliveryTime: getOrderTime(40)
+  },
+  {
+    orderId: 'OR100003',
+    productList: [
+      {
+        productId: 'PR100002',
+        productName: 'product2',
+        quantity: '3'
+      }
+    ],
+    orderPlacementDate: 'August 15, 2017',
+    value: '15 £',
+    status: 'closed',
+    deliveryTime: getOrderTime(25)
+  },
+  {
+    orderId: 'OR100004',
+    productList: [
+      {
+        productId: 'PR100001',
+        productName: 'product1',
+        quantity: '4'
+      }
+    ],
+    orderPlacementDate: 'September 2, 2017',
+    value: '40 £',
+    status: 'open',
+    deliveryTime: getOrderTime(30)
+  },
+  {
+    orderId: 'OR100005',
+    productList: [
+      {
+        productId: 'PR100001',
+        productName: 'product1',
+        quantity: '2'
+      },
+      {
+        productId: 'PR100002',
+        productName: 'product2',
+        quantity: '3'
+      },
+      {
+        productId: 'PR100003',
+        productName: 'product3',
+        quantity: '5'
+      }
+    ],
+    orderPlacementDate: 'September 12, 2017',
+    value: '90 £',
+    status: 'open',
+    deliveryTime: getOrderTime(50)
+  }
+]
---
+++ shoppingList.js
@@ -0,0 +1,101 @@
+exports.shoppingList = {
+
+  daily: {
+    productList: [
+      {
+        productId: 'PR100003',
+        productName: 'product3',
+        quantity: '3'
+      }
+    ],
+    status: 'resume'
+  },
+
+  weekly: {
+    productList: [
+      {
+        productId: 'PR100034',
+        productName: 'product34',
+        quantity: '3'
+      },
+      {
+        productId: 'PR100035',
+        productName: 'product35',
+        quantity: '4'
+      },
+      {
+        productId: 'PR100036',
+        productName: 'product36',
+        quantity: '1'
+      }
+    ],
+    status: 'resume'
+  },
+
+  monthly: {
+    productList: [
+      {
+        productId: 'PR100030',
+        productName: 'product30',
+        quantity: '8'
+      },
+      {
+        productId: 'PR100031',
+        productName: 'product31',
+        quantity: '3'
+      },
+      {
+        productId: 'PR100039',
+        productName: 'product39',
+        quantity: '12'
+      },
+      {
+        productId: 'PR100013',
+        productName: 'product13',
+        quantity: '7'
+      }
+    ],
+    status: 'resume'
+  },
+
+  yearly: {
+    productList: [
+      {
+        productId: 'PR100030',
+        productName: 'product30',
+        quantity: '8'
+      },
+      {
+        productId: 'PR100031',
+        productName: 'product31',
+        quantity: '3'
+      },
+      {
+        productId: 'PR100039',
+        productName: 'product39',
+        quantity: '12'
+      },
+      {
+        productId: 'PR100013',
+        productName: 'product13',
+        quantity: '7'
+      },
+      {
+        productId: 'PR100034',
+        productName: 'product34',
+        quantity: '3'
+      },
+      {
+        productId: 'PR100035',
+        productName: 'product35',
+        quantity: '4'
+      },
+      {
+        productId: 'PR100036',
+        productName: 'product36',
+        quantity: '1'
+      }
+    ],
+    status: 'resume'
+  }
+}
