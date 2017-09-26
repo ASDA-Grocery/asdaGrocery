2b0e798008f4ce5c87da77856349f05d7b0bd9a9
ASDA-Grocery <32156105+ASDA-Grocery@users.noreply.github.com>
2017-09-26 14:41:00 +0530

    Changed quantity type from string to number

--- shoppingList.js
+++ shoppingList.js
@@ -5,7 +5,7 @@ exports.shoppingList = {
       {
         productId: 'PR100003',
         productName: 'product3',
-        quantity: '3'
+        quantity: 3
       }
     ],
     status: 'resume'
@@ -16,17 +16,17 @@ exports.shoppingList = {
       {
         productId: 'PR100034',
         productName: 'product34',
-        quantity: '3'
+        quantity: 3
       },
       {
         productId: 'PR100035',
         productName: 'product35',
-        quantity: '4'
+        quantity: 4
       },
       {
         productId: 'PR100036',
         productName: 'product36',
-        quantity: '1'
+        quantity: 1
       }
     ],
     status: 'resume'
@@ -37,22 +37,22 @@ exports.shoppingList = {
       {
         productId: 'PR100030',
         productName: 'product30',
-        quantity: '8'
+        quantity: 8
       },
       {
         productId: 'PR100031',
         productName: 'product31',
-        quantity: '3'
+        quantity: 3
       },
       {
         productId: 'PR100039',
         productName: 'product39',
-        quantity: '12'
+        quantity: 12
       },
       {
         productId: 'PR100013',
         productName: 'product13',
-        quantity: '7'
+        quantity: 7
       }
     ],
     status: 'resume'
@@ -63,37 +63,37 @@ exports.shoppingList = {
       {
         productId: 'PR100030',
         productName: 'product30',
-        quantity: '8'
+        quantity: 8
       },
       {
         productId: 'PR100031',
         productName: 'product31',
-        quantity: '3'
+        quantity: 3
       },
       {
         productId: 'PR100039',
         productName: 'product39',
-        quantity: '12'
+        quantity: 12
       },
       {
         productId: 'PR100013',
         productName: 'product13',
-        quantity: '7'
+        quantity: 7
       },
       {
         productId: 'PR100034',
         productName: 'product34',
-        quantity: '3'
+        quantity: 3
       },
       {
         productId: 'PR100035',
         productName: 'product35',
-        quantity: '4'
+        quantity: 4
       },
       {
         productId: 'PR100036',
         productName: 'product36',
-        quantity: '1'
+        quantity: 1
       }
     ],
     status: 'resume'
