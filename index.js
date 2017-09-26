a065998f28ca69ee446aebf23508503620dee797
ASDA-Grocery <32156105+ASDA-Grocery@users.noreply.github.com>
2017-09-26 16:55:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(bodyParser.json());
 
 
 app.post('/enquireOrder', function(req, res) {  
-    var accessToken = req.body.data.user.accessToken;
+    var accessToken = req.body;
     console.log('access token - > ',accessToken);
     oauth2Client.credentials = accessToken;
     var speech
