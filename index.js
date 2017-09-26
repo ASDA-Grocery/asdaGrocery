b0c702cb4a84630fbd098037b34b20c1bdd51cba
ASDA-Grocery <32156105+ASDA-Grocery@users.noreply.github.com>
2017-09-26 17:16:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -225,6 +225,7 @@ app.post('/enquireOrder', function(req, res) {
     
     else if(intent==='changeDeliveryDate'){
         console.log('intent - > ', intent);
+        listEvents(oauth2Client);
     }
     
     else{
@@ -238,6 +239,32 @@ app.post('/enquireOrder', function(req, res) {
     });
 });
 
+function listEvents(auth) {
+    var calendar = google.calendar('v3');
+    calendar.events.list({
+        auth: auth,
+        calendarId: 'primary',
+        timeMin: (new Date()).toISOString(),
+        maxResults: 10,
+        singleEvents: true,
+        orderBy: 'startTime'
+    }, function(err, response) {
+        if (err) {
+          console.log('The API returned an error: ' + err);
+          return;
+        }
+        var events = response.items;
+        if (events.length == 0) {
+          console.log('No upcoming events found.');
+        } else {
+          console.log('Upcoming 10 events:');
+          for (var i = 0; i < events.length; i++) {
+            var event = events[i];
+            var start = event.start.dateTime || event.start.date;
+            console.log('%s - %s', start, event.summary);
+          }
+    });
+}
 app.listen((process.env.PORT || 8000), function() {
     console.log("Server up and listening");
 });
