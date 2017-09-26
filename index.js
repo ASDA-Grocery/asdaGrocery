d03ca16fd7052b4a57203da611fdd37ca9e5ae33
ASDA-Grocery <32156105+ASDA-Grocery@users.noreply.github.com>
2017-09-26 17:29:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -225,7 +225,8 @@ app.post('/enquireOrder', function(req, res) {
     
     else if(intent==='changeDeliveryDate'){
         console.log('intent - > ', intent);
-        listEvents(oauth2Client);
+        console.log('oauth - > ', oauth2Client);
+//         listEvents(oauth2Client);
     }
     
     else{
