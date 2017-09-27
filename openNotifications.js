2f6cba66c1e2a458ba897e4a0be747564aad95a9
ASDA-Grocery <32156105+ASDA-Grocery@users.noreply.github.com>
2017-09-27 10:28:28 +0530

    Update openNotifications.js

--- openNotifications.js
+++ openNotifications.js
@@ -2,11 +2,7 @@ exports.openNotifications = {
   remindProductsBackInStock : [
     {
       productId: 'PR00056',
-      productName: 'Ainsley Harriott Sumdried Tomato'
-    },
-    {
-      productId: 'PR00050',
-      productName: 'Garlic Cous Cous 100 grams'
+      productName: 'Maple loops 350 grams’'
     }
   ]
 }
