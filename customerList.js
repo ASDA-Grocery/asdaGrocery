98e35aa58ec838cffbe0c38a8b2fdcda620dc23b
ASDA-Grocery <32156105+ASDA-Grocery@users.noreply.github.com>
2017-09-29 11:27:32 +0530

    Add files via upload

---
+++ customerList.js
@@ -0,0 +1,52 @@
+exports.customerList = [
+  {
+    customerId: 'CU100239',
+    customerName: 'Barry Allen',
+    address: 'ABC Bunglow, Bangalore'
+  },
+  {
+    customerId: 'CU100240',
+    customerName: 'Harry Potter',
+    address: 'PQR Bunglow, Indore'
+  },
+  {
+    customerId: 'CU100241',
+    customerName: 'John Doe',
+    address: 'MMM Bunglow, Hyderabad'
+  },
+  {
+    customerId: 'CU100242',
+    customerName: 'Clark Kent',
+    address: 'GHT Bunglow, Pune'
+  },
+  {
+    customerId: 'CU100243',
+    customerName: 'Bruce Wayne',
+    address: 'THM Bunglow, Gurgaon'
+  },
+  {
+    customerId: 'CU100244',
+    customerName: 'Tony Stark',
+    address: 'LOP Bunglow, Delhi'
+  },
+  {
+    customerId: 'CU100245',
+    customerName: 'Arthur Curry',
+    address: 'LOL Bunglow, Mumbai'
+  },
+  {
+    customerId: 'CU100246',
+    customerName: 'Diana Prince',
+    address: 'HAH Bunglow, Kolkata'
+  },
+  {
+    customerId: 'CU100247',
+    customerName: 'Natasha Romenoff',
+    address: 'ENG Bunglow, Lucknow'
+  },
+  {
+    customerId: 'CU100248',
+    customerName: 'Iris West',
+    address: 'QWE Bunglow, Chennai'
+  }
+]
---
+++ productList.js
@@ -0,0 +1,72 @@
+exports.productList = [
+  {
+    productId: 'PR100239',
+    productName: 'NATURE VALLEY COCONUT ALMOND BAR',
+    productType: 'bar',
+    protein: 10,
+    fat: 1.2
+  },
+  {
+    productId: 'PR100240',
+    productName: 'SIMPLY PROTEIN BAR',
+    productType: 'bar',
+    protein: 10,
+    fat: 1.5
+  },
+  {
+    productId: 'PR100241',
+    productName: 'COMBAT CRUNCH BAR',
+    productType: 'bar',
+    protein: 10,
+    fat: 2.2
+  },
+  {
+    productId: 'PR100242',
+    productName: 'OMEGA CHOCOLATE BAR',
+    productType: 'bar',
+    protein: 20,
+    fat: 0.5
+  },
+  {
+    productId: 'PR100243',
+    productName: 'QUEST NUTRITION BAR',
+    productType: 'bar',
+    protein: 20,
+    fat: 1.2
+  },
+  {
+    productId: 'PR100244',
+    productName: 'YOGABARS PROTEIN BAR',
+    productType: 'bar',
+    protein: 6,
+    fat: 1.2
+  },
+  {
+    productId: 'PR100245',
+    productName: 'HYP PROTEIN BAR',
+    productType: 'bar',
+    protein: 5,
+    fat: 0.7
+  },
+  {
+    productId: 'PR100246',
+    productName: 'HYP LEAN PROTEIN BAR',
+    productType: 'bar',
+    protein: 5,
+    fat: 0.8
+  },
+  {
+    productId: 'PR100247',
+    productName: 'CALIBAR GOFIT PROTEIN BAR',
+    productType: 'bar',
+    protein: 7.7,
+    fat: 1.0
+  },
+  {
+    productId: 'PR100248',
+    productName: 'RITE BITE PROTEIN BAR',
+    productType: 'bar',
+    protein: 7,
+    fat: 0.3
+  }
+]
