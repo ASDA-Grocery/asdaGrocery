e7ef764bfbfb90519bd22d135f3caa3943f85aa1
ASDA-Grocery <32156105+ASDA-Grocery@users.noreply.github.com>
2017-10-04 11:20:17 +0530

    Update orderDb.js

--- orderDb.js
+++ orderDb.js
@@ -63,7 +63,7 @@ exports.orderDb = [
     ],
     orderPlacementDate: 'September 2, 2017',
     value: '40 £',
-    status: 'closed',
+    status: 'open',
     deliveryTime: getOrderTime(30)
   },
   {
